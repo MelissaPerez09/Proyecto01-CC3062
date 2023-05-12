dbbe8793c354651cdce63beb6224ed82b7a4dcf7
Melissa Pérez <per21385@uvg.edu.gt>
2023-05-11 18:46:29 -0600

    First component finished

---
+++ design-crafter/src/components/Global/Global.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header'
+import First from '../First/First'
+
+function Global() {
+  return (
+    <div>
+      <Header/>
+      <First/>
+    </div>
+  )
+}
+
+export default Global
