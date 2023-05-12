7fef6ae6924852183d7dbad71f37b6f104b5a243
Melissa Pérez <per21385@uvg.edu.gt>
2023-05-11 19:44:03 -0600

    cards implemented

--- design-crafter/src/components/Global/Global.jsx
+++ design-crafter/src/components/Global/Global.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Header/Header'
 import First from '../First/First'
+import Cards from '../Cards/Cards'
 
 function Global() {
   return (
     <div>
       <Header/>
       <First/>
+      <Cards/>
     </div>
   )
 }
