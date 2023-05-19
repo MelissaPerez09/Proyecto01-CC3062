f2093778293034c7e3b5ad0839f55e8956ed53a3
Melissa Pérez <per21385@uvg.edu.gt>
2023-05-18 23:05:25 -0600

    footer tested with storybook

---
+++ src/components/Footer/Footer.stories.js
@@ -0,0 +1,17 @@
+import Footer from './Footer'
+import styles from './Footer.module.css'
+
+export default {
+  title: 'Footer/all-space',
+  component: Footer,
+  tags: ['autodocs'],
+  argTypes: {
+  },
+}
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+}
